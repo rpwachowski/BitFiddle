64eae8af922d4820f44438e4b0fb9e524f5658aa
Ryan Wachowski <ryan@Ryans-Mac-mini.local>
2018-03-26 02:14:15 -0400

    Adds deleting tests

--- BitFiddleTests/Logic/ImmutableWriteTests.swift
+++ BitFiddleTests/Logic/ImmutableWriteTests.swift
@@ -15,27 +15,53 @@ class ImmutableWriteTests: XCTest {
         XCTAssert(binary == result)
     }
     
-    func testOverwriteFromBeginningOfBinary() {
+    func testOverwritingFromBeginningOfBinary() {
         let replacement = Array(repeating: Byte.min, count: 10)
         let result = binary.overwriting(with: replacement, between: .zero, and: .init(stride: 10))
         XCTAssert(result == Binary(bytes: replacement + Array(repeating: Byte.max, count: 10)))
     }
     
-    func testOverwriteToEndOfBinary() {
+    func testOverwritingToEndOfBinary() {
         let replacement = Array(repeating: Byte.min, count: 10)
-        let result = binary.overwriting(with: replacement, between: .init(stride: 10), and: .init(stride: 21))
+        let result = binary.overwriting(with: replacement, between: .init(stride: 10), and: .init(stride: 20))
         XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 10) + replacement))
     }
     
-    func testOverwriteWithinBinary() {
+    func testOverwritingWithinBinary() {
         let replacement = Array(repeating: Byte.min, count: 10)
-        let result = binary.overwriting(with: replacement, between: .init(stride: 5), and: .init(stride: 16))
+        let result = binary.overwriting(with: replacement, between: .init(stride: 5), and: .init(stride: 15))
         XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 5) + replacement + Array(repeating: Byte.max, count: 5)))
+    }
+    
+    func testOverwritingAllBinary() {
+        let replacement = Array(repeating: Byte.min, count: 20)
+        let result = binary.overwriting(with: replacement, between: .zero, and: .init(stride: 20))
+        XCTAssert(result == Binary(bytes: replacement))
     }
     
     func testDeletingEmptyReturnsEquivalentBinary() {
         let result = binary.deleting(between: .zero, and: .zero)
         XCTAssert(binary == result)
+    }
+    
+    func testDeletingFromBeginningOfBinary() {
+        let result = binary.deleting(between: .zero, and: .init(stride: 10))
+        XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 10)))
+    }
+    
+    func testDeletingToEndOfBinary() {
+        let result = binary.deleting(between: .zero, and: .init(stride: 10))
+        XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 10)))
+    }
+    
+    func testDeletingWithinBinary() {
+        let result = binary.deleting(between: .init(stride: 5), and: .init(stride: 15))
+        XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 10)))
+    }
+    
+    func testDeletingAllBinary() {
+        let result = binary.deleting(between: .zero, and: .init(stride: 20))
+        XCTAssert(result == Binary(bytes: []))
     }
     
 }
