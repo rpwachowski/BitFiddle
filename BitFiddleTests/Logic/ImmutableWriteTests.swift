06bd7c5e65234e07f8d79b2c2805b299ef7c922e
Ryan Wachowski <ryan@Ryans-Mac-mini.local>
2018-03-26 01:17:56 -0400

    Adds more overwrite tests

--- BitFiddleTests/Logic/ImmutableWriteTests.swift
+++ BitFiddleTests/Logic/ImmutableWriteTests.swift
@@ -16,6 +16,24 @@ class ImmutableWriteTests: XCTest {
         XCTAssert(binary == result)
     }
     
+    func testOverwriteFromBeginningOfBinary() {
+        let replacement = Array(repeating: Byte.min, count: 10)
+        let result = binary.overwriting(with: replacement, between: .zero, and: .init(stride: 10))
+        XCTAssert(result == Binary(bytes: replacement + Array(repeating: Byte.max, count: 10)))
+    }
+    
+    func testOverwriteToEndOfBinary() {
+        let replacement = Array(repeating: Byte.min, count: 10)
+        let result = binary.overwriting(with: replacement, between: .init(stride: 10), and: .init(stride: 21))
+        XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 10) + replacement))
+    }
+    
+    func testOverwriteWithinBinary() {
+        let replacement = Array(repeating: Byte.min, count: 10)
+        let result = binary.overwriting(with: replacement, between: .init(stride: 5), and: .init(stride: 16))
+        XCTAssert(result == Binary(bytes: Array(repeating: Byte.max, count: 5) + replacement + Array(repeating: Byte.max, count: 5)))
+    }
+    
     func testDeletingEmptyReturnsEquivalentBinary() {
         let result = binary.deleting(between: .zero, and: .zero)
         XCTAssert(binary == result)
